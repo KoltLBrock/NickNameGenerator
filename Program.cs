ba728cb043929b08a8eda2d149e8e936655ac8b2
KoltLBrock <35542746+KoltLBrock@users.noreply.github.com>
2018-01-28 14:08:15 -0600

    Real Initial Commit

--- Program.cs
+++ Program.cs
@@ -6,7 +6,16 @@ namespace NickNameGenerator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[,] nicknames = new string[10,2];
+            nicknames[0,0] = "Dude";
+            nicknames[0,1] = "Jones";
+
+            Console.WriteLine("Enter your name please: ");
+            string name = Console.ReadLine();
+            string[] nameparts = name.Split(new char[' ']);
+            for(int i=0; i < nameparts.Length; i++){
+                Console.WriteLine(nameparts[i]);
+            }
         }
     }
 }
